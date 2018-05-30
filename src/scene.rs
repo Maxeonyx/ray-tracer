e465e441e717ad5dbcae9593b71576cb0c78ff8e
maxeonyx <maxeonyx@gmail.com>
2018-05-30 21:12:42 +1200

    change the scene to just a ball

--- src/scene.rs
+++ src/scene.rs
@@ -16,84 +16,34 @@ impl Scene {
     }
 
     pub fn initialise() -> Scene {
+
+        let ball = Shape::Sphere(Sphere {
+            center: V3 {
+                x: 0.0,
+                y: 0.0,
+                z: -5.0,
+            },
+            radius: 4.0,
+        });
+
         Scene {
-            objects: vec![
-                Object2 {
-                    color: V3 {
-                        x: 0.1,
-                        y: 0.8,
-                        z: 0.1,
-                    },
-                    shape: Shape::Sphere(Sphere {
-                        center: V3 {
-                            x: 0.0,
-                            y: 0.0,
-                            z: -5.0,
-                        },
-                        radius: 1.0,
-                    }),
-                    surface: Surface::Diffuse,
+            objects: vec![Object2 {
+                color: V3 {
+                    x: 0.6,
+                    y: 0.6,
+                    z: 0.9,
                 },
-                Object2 {
-                    color: V3 {
-                        x: 0.3,
-                        y: 0.3,
-                        z: 0.6,
-                    },
-                    shape: Shape::Triangle(Triangle {
-                        vertex_1: V3 {
-                            x: 0.0,
-                            y: -1.0,
-                            z: -6.0,
-                        },
-                        vertex_2: V3 {
-                            x: 0.0,
-                            y: -3.0,
-                            z: -6.0,
-                        },
-                        vertex_3: V3 {
-                            x: 3.0,
-                            y: -3.5,
-                            z: -12.0,
-                        },
-                    }),
-                    surface: Surface::Diffuse,
+                surface: Surface::Diffuse,
+                shape: ball,
+            }],
+            lights: vec![Light {
+                position: V3 {
+                    x: -2.0,
+                    y: 3.0,
+                    z: -0.5,
                 },
-                Object2 {
-                    color: V3 {
-                        x: 0.8,
-                        y: 0.4,
-                        z: 0.1,
-                    },
-                    shape: Shape::Sphere(Sphere {
-                        center: V3 {
-                            x: 0.0,
-                            y: 3.0,
-                            z: -10.0,
-                        },
-                        radius: 10.0,
-                    }),
-                    surface: Surface::Diffuse,
-                },
-            ],
-            lights: vec![
-                Light {
-                    position: V3 {
-                        x: 6.0,
-                        y: -6.0,
-                        z: 0.0,
-                    },
-                    brightness: 10.0,
-                },
-                Light {
-                    position: V3 {
-                        x: -6.0,
-                        y: -6.0,
-                        z: 0.0,
-                    },
-                    brightness: 3.0,
-                },
-            ],
+                brightness: 100.0,
+            }],
         }
     }
 }
